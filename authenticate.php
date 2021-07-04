dd7bfe3e2f0a41ed3d19be371857af0bcb0a49cb
IllusoryLabs <jaa.judeaugust@gmail.com>
2021-07-04 11:11:25 -0400

    modified:   authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 // Change this to your connection info.
-$DATABASE_HOST = 'https://illusorylabs.github.io/dolist/';
+$DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_PASS = '6GR33NFru1tL00ps';
 $DATABASE_NAME = 'phplogin';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
