b4853e53eb763a59fffba7f72e1be5437b33de47
IllusoryLabs <jaa.judeaugust@gmail.com>
2021-07-04 10:37:37 -0400

    modified:   authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
+$DATABASE_HOST = 'do-list.me';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
