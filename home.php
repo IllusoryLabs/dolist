1c973ce5923b0a0904eeae372735953b0755c315
IllusoryLabs <jaa.judeaugust@gmail.com>
2021-07-04 15:26:39 -0400

    git 	modified:   home.php 	modified:   logout.php 	modified:   profile.php

--- home.php
+++ home.php
@@ -20,6 +20,7 @@ if (!isset($_SESSION['loggedin'])) {
 		<nav class="navtop">
 			<div>
 				<h1>DoList</h1>
+				<a href="home.php"><i class="fas fa-home"></i>Home</a>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
